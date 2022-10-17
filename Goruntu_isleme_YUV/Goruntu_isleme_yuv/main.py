cb948d1362523503876bc7b014e6d976f00b60f4
Muhammed-Taha-Bolukbasi <115821842+Muhammed-Taha-Bolukbasi@users.noreply.github.com>
2022-10-17 19:55:49 +0300

    Goruntu_Isleme_YUV
    Goruntu_Isleme_YUV

--- Goruntu_isleme_YUV/Goruntu_isleme_yuv/main.py
+++ Goruntu_isleme_YUV/Goruntu_isleme_yuv/main.py
@@ -10,6 +10,10 @@ B = foto[:,:,0]
 G = foto[:,:,1]
 R = foto[:,:,2]
 
+cv2.imshow("Mavi",B)
+cv2.imshow("Yesil",G)
+cv2.imshow("Kirmizi",R)
+
 imgGray = 0.2989 * R + 0.5870 * G + 0.1140 * B
 plt.imshow(imgGray, cmap='gray')
 plt.show()
